17d021dc029c6c60bbad78ff9483ae870c10e6a6
SCLinDennis <dennis60512@gmail.com>
2016-12-17 21:46:42 +0800

    Update selectwindow_checker.py
    There are still some problems need to be solved

--- selectwindow_checker.py
+++ selectwindow_checker.py
@@ -40,23 +40,29 @@ def selectwindow(record_info_clear):
     for i in range(row_record_info_clear-1):
         if (record_info_clear[i+1,0] != record_info_clear[i,0]):
             if checkpoint == 0:
-                if np.linalg.norm(record_info_clear[i+1,1:5]-record_info_clear[i,1:5]) <= 30:
+                center_dist = np.linalg.norm((record_info_clear[i+1,1]+record_info_clear[i+1,3])-(record_info_clear[i,1]+record_info_clear[i,3]))
+                if center_dist <= 5:
                     person_A_temp = record_info_clear[i+1]
                     person_A.append(person_A_temp)
                     person_A_info = np.array(person_A)
 #                    index_A = index_A + 1
                     checkpoint = 0
+                elif (center_dist > 5) and (center_dist <= 40):
+                    record_info_clear[i+1,1:5]  = record_info_clear[i,1:5]                    
                 else:
                     person_B_temp = record_info_clear[i+1]
                     person_B.append(person_B_temp)
                     person_B_info = np.array(person_B)
                     checkpoint = 1 
             else: 
-                if np.linalg.norm(record_info_clear[i+1,1:5]-record_info_clear[i,1:5]) <= 30:
+                center_dist = np.linalg.norm((record_info_clear[i+1,1]+record_info_clear[i+1,3])-(record_info_clear[i,1]+record_info_clear[i,3]))
+                if  center_dist <= 5:
                     person_B_temp = record_info_clear[i+1]
                     person_B.append(person_B_temp)
                     person_B_info = np.array(person_B)
                     checkpoint = 1 
+                elif (center_dist > 5) and (center_dist <= 40):
+                    record_info_clear[i+1,1:5]  = record_info_clear[i,1:5]                     
                 else:
                     person_A_temp = record_info_clear[i+1]
                     person_A.append(person_A_temp)
@@ -77,7 +83,8 @@ def selectwindow(record_info_clear):
     return (person_A_info, person_B_info)
 AnswerA = np.array([])
 AnswerB = np.array([])
-record_info_clear = unique_rows(record_info)
+#record_info_clear = unique_rows(record_info)
+record_info_clear = np.load('/Users/DennisLin/tmp/record_info_clear.npy')
 (AnswerA,AnswerB) = selectwindow(record_info_clear)      
 
 
@@ -120,19 +127,22 @@ while(camera.isOpened()):
             cv2.rectangle(frame, (AnswerA[i,1],AnswerA[i,2]), (AnswerA[i,3], AnswerA[i,4]), (0, 255, 0), 2)
     for i in range(AnswerB.shape[0]) :
         if frame_num == AnswerB[i,0]:
-            cv2.rectangle(frame, (AnswerB[i,1],AnswerB[i,2]), (AnswerB[i,3], AnswerB[i,4]), (0, 255, 255), 2)            
-    for e in range(width):
-        if row[e,0] == frame_num:
-            (x,y) = (row[e,1], row[e,2])
-            for (x_rect, y_rect, w_rect, h_rect) in rects:
-                if (x_rect < int(x*r)) and (int(x*r) < (x_rect+w_rect)) and (y_rect < int(y*r)) and (int(y*r) < (y_rect+h_rect)):
-                    cv2.circle(orig_resized, (int(x*r), int(y*r)), int(1),(0, 255, 255), 2)                    
+            cv2.rectangle(frame, (AnswerB[i,1],AnswerB[i,2]), (AnswerB[i,3], AnswerB[i,4]), (0, 0, 255), 2)            
+    cv2.putText(frame, "Frame_index: {}".format(frame_num), (10, 20),cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
+#
+            #    for e in range(width):
+#        if row[e,0] == frame_num:
+#            (x,y) = (row[e,1], row[e,2])
+#            for (x_rect, y_rect, w_rect, h_rect) in rects:
+#                if (x_rect < int(x*r)) and (int(x*r) < (x_rect+w_rect)) and (y_rect < int(y*r)) and (int(y*r) < (y_rect+h_rect)):
+#                    cv2.circle(orig_resized, (int(x*r), int(y*r)), int(1),(0, 255, 255), 2)                    
 
 #    cv2.rectangle(frame, (365, 130), (470, 400), (0, 255, 255), 2)
 
 #    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
     cv2.imshow('frame',frame)
+    
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
     frame_num +=1
